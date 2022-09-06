713a5693294805136dfdeb1480c3669e0abf01dd
wmaterkowska <100539669+wmaterkowska@users.noreply.github.com>
2022-09-06 21:01:40 +0200

    Completable future (#7)
    * version 3, completable future added
    
    * version 3, completable future, log info added
    
    * version 3, completable future, log info added, with corrections
    
    * Update asynchronousTaskProcessing/src/test/java/com/example/asynchronous/TaskServiceTests.java
    
    * version 3, TaskExecutor removed, task operation moved to TaskService
    
    Co-authored-by: Tunç Ikikardes <tunch.ikikardes@gmail.com>

--- asynchronousTaskProcessing/src/main/java/com/example/asynchronous/service/TaskService.java
+++ asynchronousTaskProcessing/src/main/java/com/example/asynchronous/service/TaskService.java
@@ -1,38 +1,64 @@
 package com.example.asynchronous.service;
 
 import com.example.asynchronous.data.Task;
-import com.example.asynchronous.data.TaskExecutor;
 import com.example.asynchronous.data.TaskRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.scheduling.annotation.Async;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class TaskService {
 
     private final TaskRepository taskRepository;
 
+    /**
+     * give list with all started tasks
+     * @return List of all started tasks
+     */
     public List<Task> getAllTasks() {
 
-        /*
-        Iterable<Task> tasks = this.taskRepository.findAll();
-        List<Task> taskList = new ArrayList<>();
-        tasks.forEach(task ->{taskList.add(task);});
-        return taskList;
-        */
+        log.info("list of tasks: " + this.taskRepository.findAll());
         return this.taskRepository.findAll();
     }
 
-    @Async
-    public void addTask(int base, int exponent) throws InterruptedException {
+    public void cleanRepository(){
+        this.taskRepository.deleteAll();
+    }
+
+    /**
+     * add task to the database (initiate calculating of the result of the task)
+     * @param base integer
+     * @param exponent integer
+     */
+    public void addTask(int base, int exponent) {
         final Task newTask = Task.builder().base(base).exponent(exponent).build();
         this.taskRepository.save(newTask);
 
-        final TaskExecutor newTaskExecutor = new TaskExecutor(this.taskRepository);
-
-        newTaskExecutor.calculateResult(newTask);
+        new CompletableFuture<>().runAsync( ()-> {
+            try{
+                this.calculateResult(newTask);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        log.info("task added, calculating started");
     }
+
+    public void calculateResult(Task task) throws InterruptedException {
+        long currentResult = 1;
+        for (int i = 1; i <= task.getExponent(); i++){
+            Thread.sleep(1000);
+            task.setStatus( ((float) i / task.getExponent()) * 100);
+            currentResult = task.getBase() * currentResult;
+            taskRepository.save(task);
+        }
+        task.setResult(currentResult);
+        this.taskRepository.save(task);
+    }
+
 }
